1e363ee5b5e9f9d6165b21cc8fab1b0ca6aed356
IAmNelu <ifrimvale@gmail.com>
2025-04-04 17:25:30 +0200

    feat: update lib

--- container/src/main.js
+++ container/src/main.js
@@ -6,19 +6,19 @@ import { enableLogging, MessagePeer } from "@amadeus-it-group/microfrontends";
 // Message types are optional
 const container = new MessagePeer({
   id: "container",
-  onMessage: (message) => {
-    //here you can handle the messages received from the iframe
-    // switch (message.type) {
-    //     case 'select':
-    //         console.log('From canvas in the container', message)
-    //         break;
-    //     default:
-    //         console.log('From canvas in the container', message)
-    // }
-
-    console.log("From canvas in the container", message);
-  },
   knownMessages: [{ type: "select", version: "1.0" }],
+});
+
+const subscription = container.messages.subscribe((message) => {
+  //here you can handle the messages received from the iframe
+  // switch (message.type) {
+  //     case 'select':
+  //         console.log('From canvas in the container', message)
+  //         break;
+  //     default:
+  //         console.log('From canvas in the container', message)
+  // }
+  console.log("From canvas in the container", message);
 });
 const iframe = document.querySelector("iframe#canvas");
 // You should check that there is one connection and then send the data
@@ -33,7 +33,9 @@ container
   });
 
 document.querySelector("button#button").addEventListener("click", (e) => {
+  //avoid memory leaks
   container.disconnect();
+  subscription.unsubscribe();
 });
 
 document.querySelector("button#log").addEventListener("click", (e) => {
